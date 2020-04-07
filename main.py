37e1a015a4b834d537cb4d408154f15e73b153ad
Ulvar Massive Anus <bannikovtop@gmail.com>
2020-04-07 19:18:05 +0300

    В базовом шаблоне добавлеена навигация. Построена структура проекта.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Git test")
+from flask import Flask, render_template
+from flask_login import current_user
+from data.db_session import global_init, create_session
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('home_page.html', current_user=current_user)
+
+
+if __name__ == '__main__':
+    global_init("db/project.sqlite")
+    app.run()
